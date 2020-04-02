a9c5f8e0a2780660c158b5f92ed1a29852b90718
Stepan Savchuk <stepansw99@i.ua>
2020-04-02 16:41:28 +0300

    separated fight cloud logic to fight cloud script

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -3,13 +3,13 @@ extends Actor
 export var init_num = 3
 export var init_denom = 3
 
-var fight_cloud_textures = Array()
+var cloud = null
 
 func _ready():
 	health.num = init_num
 	health.denom = init_denom
 	update_health_label()
-	load_textures()
+	cloud = get_tree().get_root().get_node("TestSpace").find_node("fight_cloud")
 
 func _physics_process(delta: float) -> void:
 	check_on_enemy()
@@ -42,11 +42,10 @@ func fight(enemy: KinematicBody2D) -> void:
 	print("Fight!")
 	fight_coroutine(enemy)
 	print("Uno")
-	
-		
+
 func fight_coroutine(enemy: KinematicBody2D) -> void:
 	setActive(false)
-	set_fight_cloud_active(true, enemy.position)
+	cloud.set_active(true, position, enemy.position)
 	enemy.setActive(false)
 	yield(get_tree().create_timer(1.0), "timeout")
 	print("Dos")
@@ -63,26 +62,4 @@ func fight_coroutine(enemy: KinematicBody2D) -> void:
 		enemy.update_health_label()
 		die()
 		enemy.setActive(true)
-	set_fight_cloud_active(false)
-		
-func set_fight_cloud_active(state: bool, enemy_pos = Vector2.ZERO) -> void:
-	var cloud = get_tree().get_root().get_node("TestSpace").find_node("fight_cloud")
-	cloud.visible = state
-	if state:
-		var cloud_pos: = Vector2.ZERO
-		cloud_pos.x = (position.x + enemy_pos.x) / 2
-		cloud_pos.y = position.y
-		cloud.position = cloud_pos + Constants.FIGHT_CLOUD_OFFSET
-		fight_cloud_coroutine()
-
-func fight_cloud_coroutine() -> void:
-	var cloud = get_tree().get_root().get_node("TestSpace").find_node("fight_cloud")
-	for i in fight_cloud_textures.size():
-		cloud.set_texture(fight_cloud_textures[i])
-		yield(get_tree().create_timer(0.25), "timeout")
-
-func load_textures() -> void:
-	fight_cloud_textures.append(preload("res://assets/fight_cloud/fight_cloud_0.png"))
-	fight_cloud_textures.append(preload("res://assets/fight_cloud/fight_cloud_1.png"))
-	fight_cloud_textures.append(preload("res://assets/fight_cloud/fight_cloud_2.png"))
-	fight_cloud_textures.append(preload("res://assets/fight_cloud/fight_cloud_3.png"))
+	cloud.set_active(false)
---
+++ src/abstract/fightCloud.gd
@@ -0,0 +1,28 @@
+extends Sprite
+
+class_name FightCloud
+
+var textures = Array()
+
+func _ready():
+	load_textures()
+
+func load_textures() -> void:
+	textures.append(preload("res://assets/fight_cloud/fight_cloud_0.png"))
+	textures.append(preload("res://assets/fight_cloud/fight_cloud_1.png"))
+	textures.append(preload("res://assets/fight_cloud/fight_cloud_2.png"))
+	textures.append(preload("res://assets/fight_cloud/fight_cloud_3.png"))
+
+func set_active(state: bool, first_actor_pos = Vector2.ZERO, second_actor_pos = Vector2.ZERO) -> void:
+	visible = state
+	if state:
+		var cloud_pos: = Vector2.ZERO
+		cloud_pos.x = (first_actor_pos.x + second_actor_pos.x) / 2
+		cloud_pos.y = (first_actor_pos.y + second_actor_pos.y) / 2
+		position = cloud_pos + Constants.FIGHT_CLOUD_OFFSET
+		fight_cloud_coroutine()
+
+func fight_cloud_coroutine() -> void:
+	for i in textures.size():
+		set_texture(textures[i])
+		yield(get_tree().create_timer(0.25), "timeout")
