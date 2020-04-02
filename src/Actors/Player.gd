34dc5c64f193e6ab65ac306b7668fd020d66098f
Stepan Savchuk <stepansw99@i.ua>
2020-04-02 15:19:29 +0300

    implemented fight cloud animation

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -3,10 +3,13 @@ extends Actor
 export var init_num = 3
 export var init_denom = 3
 
+var fight_cloud_textures = Array()
+
 func _ready():
 	health.num = init_num
 	health.denom = init_denom
 	update_health_label()
+	load_textures()
 
 func _physics_process(delta: float) -> void:
 	check_on_enemy()
@@ -70,4 +73,16 @@ func set_fight_cloud_active(state: bool, enemy_pos = Vector2.ZERO) -> void:
 		cloud_pos.x = (position.x + enemy_pos.x) / 2
 		cloud_pos.y = position.y
 		cloud.position = cloud_pos + Constants.FIGHT_CLOUD_OFFSET
-	
+		fight_cloud_coroutine()
+
+func fight_cloud_coroutine() -> void:
+	var cloud = get_tree().get_root().get_node("TestSpace").find_node("fight_cloud")
+	for i in fight_cloud_textures.size():
+		cloud.set_texture(fight_cloud_textures[i])
+		yield(get_tree().create_timer(0.25), "timeout")
+
+func load_textures() -> void:
+	fight_cloud_textures.append(preload("res://assets/fight_cloud/fight_cloud_0.png"))
+	fight_cloud_textures.append(preload("res://assets/fight_cloud/fight_cloud_1.png"))
+	fight_cloud_textures.append(preload("res://assets/fight_cloud/fight_cloud_2.png"))
+	fight_cloud_textures.append(preload("res://assets/fight_cloud/fight_cloud_3.png"))
