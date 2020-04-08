0e52e9f3be3dd68c64fb15f62723dcde3ee9c3f1
Stepan Savchuk <stepansw99@i.ua>
2020-04-08 21:17:57 +0300

    implemented time separating on minutes

--- src/abstract/UIDisplayHealth.gd
+++ src/abstract/UIDisplayHealth.gd
@@ -3,13 +3,16 @@ extends CanvasLayer
 class_name UIDisplayHealth
 
 var progress: = 0
-var time: = 0.0
+var time: = 0
 var is_game_over: = false
+
+const MINUTE: = 60
 
 const TIMER_STEP: = 1.0
 
 func _ready():
-	timer_coroutine()
+	var timer = get_node("Timer")
+	timer_coroutine(timer)
 
 func update_label(num: int, denom: int) -> void:
 	var text = str(num) + '/' + str(denom)
@@ -19,9 +22,14 @@ func update_status(value: float) -> void:
 	progress = int(value * 1000.0)
 	get_node("HealthStatus").value = progress
 
-func timer_coroutine() -> void:
+func timer_coroutine(timer) -> void:
 	while !is_game_over:
-		get_node("Timer").text = str(time) + ".0"
+		var minutes: = int(time / MINUTE)
+		timer.text = ""
+		if minutes > 0:
+			timer.text += str(minutes) + "m "
+		var seconds = time - (minutes * MINUTE)
+		timer.text += str(seconds) + "s"
 		time += TIMER_STEP
 		yield(get_tree().create_timer(TIMER_STEP), "timeout")
 
